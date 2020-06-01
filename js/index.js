69d5525545e14393995a4f3a36d5013c078ad50e
Tania <tania.cuchetti@gmail.com>
2020-06-01 12:53:38 -0300

    backup before changes

--- js/index.js
+++ js/index.js
@@ -1,26 +1,68 @@
-
+/* 
 
 
 $(document).ready(function() {
     $('.js-series').slick({
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        arrows:false,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'), 
       });
       
-      var filtered = false;
-      
-      $('.js-filter').on('click', function(){
-        if (filtered === false) {
-          $('.filtering').slick('slickFilter',':even');
-          $(this).text('Unfilter Slides');
-          filtered = true;
-        } else {
-          $('.filtering').slick('slickUnfilter');
-          $(this).text('Filter Slides');
-          filtered = false;
-        }
-      });
+    
                       
+      responsive: [
+        { breakpoint: 1024, settings: {
+            
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }},
+        { breakpoint: 800, settings: {
+            
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }},
+        { breakpoint: 768, settings: {
+          
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }},
+        { breakpoint: 600, settings: "unslick"},
+        { breakpoint: 480, settings: "unslick"},
+         { breakpoint: 360, settings: "unslick"},
+         { breakpoint: 320, settings: {
+          
+          slidesToShow: 1,
+          slidesToScroll: 1
+         }}
+    ]
+}); */
 
-});
 
+$slick_slider = $('.js-series');
+  settings = {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows:true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+  }
+  $slick_slider.slick(settings);
+
+  
+
+  // reslick only if it's not slick()
+  $(window).on('resize', function() {
+    if ($(window).width() < 768) {
+      if ($slick_slider.hasClass('slick-initialized')) {
+        $slick_slider.slick('unslick');
+      }
+      return
+    }
+
+    if (!$slick_slider.hasClass('slick-initialized')) {
+      return $slick_slider.slick(settings);
+    }
+    
+  });
