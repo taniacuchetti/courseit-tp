310f9e231cd963eccff7499d2fa0fd2db95daf81
Tania <tania.cuchetti@gmail.com>
2020-06-05 13:34:04 -0300

    fixed slick bugs

--- js/index.js
+++ js/index.js
@@ -1,68 +1,25 @@
-/* 
-
-
-$(document).ready(function() {
-    $('.js-series').slick({
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        arrows:false,
-        prevArrow: $('.prev'),
-        nextArrow: $('.next'), 
-      });
-      
-    
-                      
-      responsive: [
-        { breakpoint: 1024, settings: {
-            
-            slidesToShow: 2,
-            slidesToScroll: 2
-        }},
-        { breakpoint: 800, settings: {
-            
-            slidesToShow: 1,
-            slidesToScroll: 1
-        }},
-        { breakpoint: 768, settings: {
-          
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }},
-        { breakpoint: 600, settings: "unslick"},
-        { breakpoint: 480, settings: "unslick"},
-         { breakpoint: 360, settings: "unslick"},
-         { breakpoint: 320, settings: {
-          
-          slidesToShow: 1,
-          slidesToScroll: 1
-         }}
-    ]
-}); */
-
-
-$slick_slider = $('.js-series');
-  settings = {
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows:true,
-    prevArrow: $('.prev'),
-    nextArrow: $('.next'),
-  }
-  $slick_slider.slick(settings);
-
-  
-
-  // reslick only if it's not slick()
-  $(window).on('resize', function() {
-    if ($(window).width() < 768) {
-      if ($slick_slider.hasClass('slick-initialized')) {
-        $slick_slider.slick('unslick');
+$('.js-series').slick({
+  slidesToShow: 4,
+  slidesToScroll: 3,
+  arrows:false,
+  draggable:true,
+  infinite: true,
+  touchThreshold:100,
+  accesibility:true,
+  swipeToSlide: true,
+  variableWidth:true,
+  responsive: [
+    {
+      breakpoint: 600, 
+        settings: {
+        arrows:false, 
+        variableWidth:true, 
+        slidesToShow: 2,
+        slidesToScroll: 2
       }
-      return
-    }
-
-    if (!$slick_slider.hasClass('slick-initialized')) {
-      return $slick_slider.slick(settings);
-    }
-    
-  });
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
