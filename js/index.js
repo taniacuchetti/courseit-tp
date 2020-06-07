2d59850a63c4a9c8d73eea117517148531ba36fe
Tania <tania.cuchetti@gmail.com>
2020-06-07 00:52:17 -0300

    Finished

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
+
+
 $('.js-series').slick({
-  slidesToShow: 4,
+  slidesToShow: 3,
   slidesToScroll: 3,
   arrows:false,
   draggable:true,
@@ -11,12 +13,36 @@ $('.js-series').slick({
   responsive: [
     {
       breakpoint: 600, 
-        settings: {
+      settings: {
         arrows:false, 
         variableWidth:true, 
         slidesToShow: 2,
         slidesToScroll: 2
-      }
+      } 
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+
+
+$('.js-series-series').slick({
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows:false,
+  draggable:true,
+  infinite: true,
+  touchThreshold:100,
+  accesibility:true,
+  swipeToSlide: true,
+  variableWidth:true,
+  responsive: [
+    {
+      breakpoint: 600, 
+        settings: "unslick",
     },
     // You can unslick at a given breakpoint now by adding:
     // settings: "unslick"
